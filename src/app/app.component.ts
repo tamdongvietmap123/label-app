bace3b8a9d8916372d743b45e9895491f5adb5a6
Tam Dong <tamdna@vietmap.vn>
2022-07-18 18:00:19 +0700

    18/07/2022 dynamic form by text splitted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,58 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormControl, FormArray } from '@angular/forms';
+import { ApiService } from './services/api.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'label-app';
+
+  public productForm !: FormGroup // reactive form angular
+  actionBtn: string = "Save" // button render variable
+  parseList!: string[] // sentences parsed => string 
+  isSubmit!: boolean
+  sentence_title!:string
+
+  constructor(
+    private _formBuilder: FormBuilder, private _apiService: ApiService) {
+
+  }
+
+  ngOnInit() {
+    this.productForm = new FormGroup({}) // create a FromGroup
+    this._apiService.getText().subscribe({
+      next: (res) => {
+        this.sentence_title = res.text
+        this._createFromGroup(res)
+      },
+      error: err => alert(err)
+    })
+
+  }
+
+  // CREATE DYNAMIC FORM
+  private _createFromGroup = (resFromGetText: any) => {
+    this.parseList = this.splitStr(resFromGetText.text)
+    for (let item of this.parseList) {
+      this.productForm.addControl(item, new FormControl(`what is ${item}`, [Validators.required]))
+    }
+  }
+
+  splitStr(str: string) {
+    let temp = str.replace(/,/g, '')
+    temp = temp.replace(/[!,?,-]/g, ' ')
+    let fields = temp.split(' ')
+    return fields
+  }
+
+  actionSubmit = () => {
+    this.productForm.valid ? alert(1) : alert(2)
+
+    if (this.productForm.valid) {
+      console.table(this.productForm.value)
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import {MatCardModule} from '@angular/material/card';
+
+import { UserService } from './services/user.service';
+import { MatNativeDateModule } from '@angular/material/core';
+import { ApiService } from './services/api.service';
 
 @NgModule({
   declarations: [
@@ -10,9 +32,28 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    FormsModule,
+    HttpClientModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatRadioModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatCheckboxModule,
+    MatAutocompleteModule,
+    MatCardModule
   ],
-  providers: [],
+  providers: [ApiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
